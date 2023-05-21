e9e1e19482323c3acdea131f5a9f4fea7f06d40d
DrKamthorn <132547814+DrKamthorn@users.noreply.github.com>
2023-05-21 18:31:16 +0700

    Update data2.py

--- data2.py
+++ data2.py
@@ -5,21 +5,11 @@ import streamlit as st
 st.set_page_config(layout="centered")
 st.title("ข้อมูลคัดกรองภาพเอกซเรย์ปอด ตั้งแต่ปี 2023")
 
-#uploaded_file = st.file_uploader("Choose a file")
-#if uploaded_file is not None:
-df = pd.read_csv('checkup.csv',on_bad_lines='skip')
-st.write(df)
+@st.cache_data(ttl=3600)
+def load_data():
+	df_froz =pd.read_csv("checkup.csv", error_bad_lines=False)
+	return df_froz
 
-  # Add some matplotlib code !
-fig, ax = plt.subplots()
-df.hist(
-  bins=8,
-  column="age",
-  grid=False,
-  figsize=(8, 8),
-  color="#86bf91",
-  zorder=2,
-  rwidth=0.9,
-  ax=ax,
-)
-st.write(fig)
+df_froz=load_data()
+num_shown=st.slider("จำนวนข้อมูลที่แสดง...",0,500,5)
+st.table(df_froz[:num_shown])
